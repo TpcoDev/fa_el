e48d66e11c858501d2b53e9614b9e5ab5d568778
WALTER MORENO <WMORENO@TPCOCL.CL>
2020-10-01 18:40:46 -0300

    valida rut y nombre comprañia version OK en EMAT

---
+++ valida_empresa_web v1/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Ahorasoft TPCO Project",
+    'category': 'TPCO',
+    'version': '1.0.0',
+    'author': "Ahorasoft",
+    'website': 'http://www.ahorasoft.com',
+    "support": "soporte@ahorasoft.com",
+    'summary': """
+        Ahorasoft TPCO Project""",
+    'description': """
+        Ahorasoft TPCO Project
+    """,
+    "images": [],
+    "depends": [
+        "website_sale"
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/as_template.xml',
+    ],
+    'qweb': [
+    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'installable': True,
+    'auto_install': False,
+    
+}
