53849923bbe5b3690eb79d46307649cababb3330
wmoreno <walter.moreno@tpco.cl>
2020-10-08 16:34:10 -0300

    corrige validacion rut y nombre cia

---
+++ valida_empresa_web/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "TPCO Project Web",
+    'category': 'TPCO',
+    'version': '1.0.1',
+    'author': "tpco",
+    'website': 'www.tpco.cl',
+    "support": "tpco@tpco.cl",
+    'summary': """
+        TPCO Project""",
+    'description': """
+        TPCO Project
+    """,
+    "images": [],
+    "depends": [
+        "website_sale"
+    ],
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/as_template.xml',
+    ],
+    'qweb': [
+    ],
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+    'installable': True,
+    'auto_install': False,
+    
+}
